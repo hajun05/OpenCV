1332a01e401179b370295113061f7dc03e85e0dc
hajun05 <hajun0325175@gmail.com>
2025-02-10 12:43:54 +0900

    실습2. OpenCvSharp 이미지 변환 Cv2.CvtColor() 메소드로 이미지를 변환. ColorConversionCodes 의 종류를 조사하고 5개의 변환 옵션을 선택.

--- SFU1_OpenCV/MainWindow.xaml.cs
+++ SFU1_OpenCV/MainWindow.xaml.cs
@@ -22,6 +22,38 @@ namespace SFU1_OpenCV
             InitializeComponent();
 
             textBox1.Text = Cv2.GetVersionString();
+
+            Mat image1 = Cv2.ImRead("C:/Users/Hajun/source/repos/OpenCV/SFU1_OpenCV/Resources/cat.jpg");
+            Mat image2 = new Mat(image1.Size(), MatType.CV_8UC1);
+            Mat image3 = new Mat(image1.Size(), MatType.CV_8UC3);
+            Mat image4 = new Mat(image1.Size(), MatType.CV_8UC3);
+            Mat image5 = new Mat(image1.Size(), MatType.CV_8UC3);
+            Mat image6 = new Mat(image1.Size(), MatType.CV_8UC3);
+            // 무슨 이유인지 상대경로 이미지 접근이 불가, OS 차이?
+            // WPF의 약점. 이미지 넣기가 어렵고 오류가 많다!
+
+            //Cv2.ImShow("Image Test", image);
+            //imageBox1.Source = OpenCvSharp.WpfExtensions.
+            //    BitmapSourceConverter.ToBitmapSource(image);
+
+            Cv2.CvtColor(image1, image2, ColorConversionCodes.RGB2GRAY);
+            Cv2.CvtColor(image1, image3, ColorConversionCodes.BGR2XYZ);
+            Cv2.CvtColor(image1, image4, ColorConversionCodes.BGR2YCrCb);
+            Cv2.CvtColor(image1, image5, ColorConversionCodes.BGR2HSV);
+            Cv2.CvtColor(image1, image6, ColorConversionCodes.BGR2Lab);
+
+            imageBox1.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image1);
+            imageBox2.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image2);
+            imageBox3.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image3);
+            imageBox4.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image4);
+            imageBox5.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image5);
+            imageBox6.Source = OpenCvSharp.WpfExtensions.
+                BitmapSourceConverter.ToBitmapSource(image6);
         }
     }
 }
